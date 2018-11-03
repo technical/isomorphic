498196acccf663947f6e8a8d6a0f9c176020cff2
Mark <sparkylewis@btinternet.com>
2018-11-03 13:08:00 +0000

    initialize

---
+++ build/precache-manifest.46d73414d24f6b9b8e0defd7005cedae.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0d838ac6dbc951c3085",
+    "url": "/app/static/js/6.f0d838ac.chunk.js"
+  },
+  {
+    "revision": "21dde21d6a68f2c515f3",
+    "url": "/app/static/js/0.21dde21d.chunk.js"
+  },
+  {
+    "revision": "adf5846b1711fa8000cfd4c6c65a411d",
+    "url": "/app/static/media/sign.adf5846b.jpg"
+  },
+  {
+    "revision": "6d84b9b2ecb7d9ab3dfd",
+    "url": "/app/static/js/main.6d84b9b2.chunk.js"
+  },
+  {
+    "revision": "f05790a212a449326732",
+    "url": "/app/static/js/3.f05790a2.chunk.js"
+  },
+  {
+    "revision": "65a124217fb08a2d2e35",
+    "url": "/app/static/js/4.65a12421.chunk.js"
+  },
+  {
+    "revision": "bf1c59a5ea8bdc59982e",
+    "url": "/app/static/js/5.bf1c59a5.chunk.js"
+  },
+  {
+    "revision": "604345b476aa52a0245ee2ffd3cd50db",
+    "url": "/app/static/media/italy.604345b4.svg"
+  },
+  {
+    "revision": "a95ebcdc83bb6de5eddd",
+    "url": "/app/static/js/1.a95ebcdc.chunk.js"
+  },
+  {
+    "revision": "5381339fd231277001f4",
+    "url": "/app/static/js/runtime~main.5381339f.js"
+  },
+  {
+    "revision": "bc48afcc15d5d9d51255de0b2ee708be",
+    "url": "/app/static/media/uk.bc48afcc.svg"
+  },
+  {
+    "revision": "4e4d59b7a0903071d743973f8e3b3aab",
+    "url": "/app/static/media/china.4e4d59b7.svg"
+  },
+  {
+    "revision": "6a9dc11ff55791478b31156cd09b6b38",
+    "url": "/app/static/media/spain.6a9dc11f.svg"
+  },
+  {
+    "revision": "bcd389debadb2cf1c0332df62b80c1b9",
+    "url": "/app/static/media/france.bcd389de.svg"
+  },
+  {
+    "revision": "6d84b9b2ecb7d9ab3dfd",
+    "url": "/app/static/css/main.a68fded7.chunk.css"
+  },
+  {
+    "revision": "f0d838ac6dbc951c3085",
+    "url": "/app/static/css/6.e8098d23.chunk.css"
+  },
+  {
+    "revision": "d2a09fcab9c6268af89ccdcac60a5207",
+    "url": "/app/index.html"
+  }
+];
